9e73fd2c92f5851ba5b7d616c93917beeb89fd9d
Vikas <vky2334@gmail.com>
2025-05-15 10:21:15 +0530

    Change Folder name

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <header className=" bg-slate-200 shadow-md p-1.5 ">
-      <div className="flex justify-between items-center max-w-6xl mx-auto ml-0.5 mr-0.5">
+      <div className="flex justify-between items-center max-w-6xl mx-auto ">
         {/* <h1 className='font-bold text-sm sm:text-xl flex-wrap'>
             <span className='text-blue-500'>Hotel</span>
             <span className='text-orange-500'>Hunt</span>
@@ -14,14 +14,14 @@ const Header = () => {
 
         <Link to='/'> <img
           src={Logo} alt="img"
-          className=" h-[40px] sm:h-[50px] sm:w-[100px]  pt-1 pb-1 cursor-pointer"
+          className=" h-[40px] sm:h-[50px] sm:w-[100px]  pt-1 pb-1 cursor-pointer "
         /></Link>
 
         <form className="bg-slate-100 rounded-3xl p-2 flex items-center cursor-pointer">
           <input
             type="text"
             placeholder="Serach..."
-            className="bg-transparent w-24 sm:w-64 focus:outline-none"
+            className="bg-transparent w-35 sm:w-90 focus:outline-none"
           />
           <FaSearch className="text-slate-500" />
         </form>
