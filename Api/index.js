00a4210225fe56b095c7c27cfd47a1e310d3157b
Vikas <vky2334@gmail.com>
2025-05-15 16:47:55 +0530

    Connection to DataBase

--- Api/index.js
+++ Api/index.js
@@ -1,8 +1,21 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to MongoDB!");
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+  }
+};
 
 const app = express();
+app.use(express.json());
 
-app.listen(8080,()=>{
-    console.log("server is runnimg")
+app.listen(8080, () => {
+  console.log("server is runnimg");
 });
-
